f735166b1c9fdae182584c76e6e33c6e82df7fbb
Andromander <cathy@feodoroff.com>
2018-01-19 12:54:23 +1000

    Fixed Crystal Growth
    Facepalms, slaps face, throws against the wall and slaps the back of past Androsa's head

--- src/main/java/androsa/gaiadimension/block/GDCrystalGrowth.java
+++ src/main/java/androsa/gaiadimension/block/GDCrystalGrowth.java
@@ -7,10 +7,13 @@ import net.minecraft.block.BlockBush;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.IShearable;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -45,9 +48,21 @@ public class GDCrystalGrowth extends BlockBush implements IShearable, ModelRegis
         return state.getBlock() == GDBlocks.glitterGrass || state.getBlock() == GDBlocks.heavySoil;
     }
 
+    @Override
+    @Deprecated
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
     @SideOnly(Side.CLIENT)
     @Override
     public BlockRenderLayer getBlockLayer() {
         return BlockRenderLayer.TRANSLUCENT;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 }
