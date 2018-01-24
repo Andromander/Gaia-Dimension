754fe7bbf9e6dfc371794f7b09e4f84a50ec4509
Andromander <cathy@feodoroff.com>
2018-01-25 08:35:10 +1000

    Ore Smelting Recipes

---
+++ src/main/java/androsa/gaiadimension/registry/GDRecipes.java
@@ -0,0 +1,26 @@
+package androsa.gaiadimension.registry;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod.EventBusSubscriber
+public class GDRecipes {
+
+    @SubscribeEvent
+    public static void ragisterRecipes(RegistryEvent.Register<IRecipe> event) {
+
+        GameRegistry.addSmelting(GDBlocks.hematiteOre, new ItemStack(GDItems.hematite, 1), 0.3F);
+        GameRegistry.addSmelting(GDBlocks.pyriteOre, new ItemStack(GDItems.hematite, 1), 0.3F);
+        GameRegistry.addSmelting(GDBlocks.cinnabarOre, new ItemStack(GDItems.hematite, 1), 0.3F);
+        GameRegistry.addSmelting(GDBlocks.labradoriteOre, new ItemStack(GDItems.hematite, 1), 0.3F);
+        GameRegistry.addSmelting(GDBlocks.moonstoneOre, new ItemStack(GDItems.hematite, 1), 0.3F);
+        GameRegistry.addSmelting(new ItemStack(GDBlocks.opalOre, 1, 0), new ItemStack(GDItems.opalRed, 1), 0.3F);
+        GameRegistry.addSmelting(new ItemStack(GDBlocks.opalOre, 1, 1), new ItemStack(GDItems.opalBlue, 1), 0.3F);
+        GameRegistry.addSmelting(new ItemStack(GDBlocks.opalOre, 1, 2), new ItemStack(GDItems.opalGreen, 1), 0.3F);
+        GameRegistry.addSmelting(new ItemStack(GDBlocks.opalOre, 1, 3), new ItemStack(GDItems.opalWhite, 1), 1.0F);
+    }
+}
