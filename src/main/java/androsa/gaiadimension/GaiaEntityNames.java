3e6ff62d0efd826f329cb6af9e954cefc36c99c8
Andromander <cathy@feodoroff.com>
2018-03-05 14:19:48 +1000

    Those builds were failing. Nobody told me this!

--- src/main/java/androsa/gaiadimension/GaiaEntityNames.java
+++ src/main/java/androsa/gaiadimension/GaiaEntityNames.java
@@ -4,6 +4,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class GaiaEntityNames {
     public static final ResourceLocation HOWLITE_WOLF = prefix("howlite_wolf");
+    public static final ResourceLocation SPELLBOUND_ELEMENTAL = prefix("spellbound_elemental");
 
     public static final ResourceLocation BLUE_HOWLITE_WOLF = prefix("blue_howlite_wolf");
 
