e20d6d177a2dc235a04161b7ea67d758c3618503
Andromander <cathy@feodoroff.com>
2018-04-07 09:24:40 +1000

    Oh, I see...

---
+++ src/main/java/androsa/gaiadimension/item/GDFlintAndGold.java
@@ -0,0 +1,49 @@
+package androsa.gaiadimension.item;
+
+
+import androsa.gaiadimension.registry.GDBlocks;
+import androsa.gaiadimension.registry.GDTabs;
+import androsa.gaiadimension.registry.ModelRegisterCallback;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class GDFlintAndGold extends Item implements ModelRegisterCallback {
+
+    public GDFlintAndGold() {
+        this.setCreativeTab(GDTabs.tabItem);
+        this.setMaxDamage(32);
+        this.maxStackSize = 1;
+    }
+
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        pos = pos.offset(facing);
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (!player.canPlayerEdit(pos, facing, itemstack)) {
+            return EnumActionResult.FAIL;
+        } else {
+            if (worldIn.isAirBlock(pos)) {
+                worldIn.playSound(player, pos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+                worldIn.setBlockState(pos, GDBlocks.goldFire.getDefaultState(), 11);
+            }
+
+            if (player instanceof EntityPlayerMP) {
+                CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, itemstack);
+            }
+
+            itemstack.damageItem(1, player);
+            return EnumActionResult.SUCCESS;
+        }
+    }
+}
