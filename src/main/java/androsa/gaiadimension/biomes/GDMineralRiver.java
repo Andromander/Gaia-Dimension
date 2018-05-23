f7963e549af0a2b1f308f5b4ff5a6ab5be9d91a7
Andromander <cathy@feodoroff.com>
2018-05-23 12:10:32 +1000

    Mineral Rivers use Salt now

--- src/main/java/androsa/gaiadimension/biomes/GDMineralRiver.java
+++ src/main/java/androsa/gaiadimension/biomes/GDMineralRiver.java
@@ -14,23 +14,7 @@ public class GDMineralRiver extends GDBiomeBase {
         super(props);
         spawnableCreatureList.clear();
 
-        topBlock = GDBlocks.glitterGrass.getDefaultState();
-        fillerBlock = GDBlocks.heavySoil.getDefaultState();
-    }
-
-    @Override
-    public void decorate(World world, Random rand, BlockPos pos) {
-        for (int l3 = 0; l3 < 4; ++l3) {
-            if (rand.nextInt(4) == 0) {
-                int i4 = rand.nextInt(16) + 8;
-                int k8 = rand.nextInt(16) + 8;
-                int j12 = world.getHeight(pos.add(i4, 0, k8)).getY() * 2;
-
-                if (j12 > 0) {
-                    int k15 = rand.nextInt(j12);
-                    new WorldGenBush((BlockBush)GDBlocks.crystalBloom).generate(world, rand, pos.add(i4, k15, k8));
-                }
-            }
-        }
+        topBlock = GDBlocks.saltBlock.getDefaultState();
+        fillerBlock = GDBlocks.saltBlock.getDefaultState();
     }
 }
