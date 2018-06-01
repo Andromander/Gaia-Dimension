8a51118182db3a3718456594193cddb4ca4c3d90
Andromander <cathy@feodoroff.com>
2018-06-01 10:45:56 +1000

    Generate Gummy Blocks in Purple Swamps

--- src/main/java/androsa/gaiadimension/biomes/GDPurpleAgateSwamp.java
+++ src/main/java/androsa/gaiadimension/biomes/GDPurpleAgateSwamp.java
@@ -3,14 +3,14 @@ package androsa.gaiadimension.biomes;
 import androsa.gaiadimension.block.GDCrystalBloom;
 import androsa.gaiadimension.block.GDCrystalGrowth;
 import androsa.gaiadimension.registry.GDBlocks;
-import androsa.gaiadimension.world.gen.GDGenCrystalBloom;
-import androsa.gaiadimension.world.gen.GDGenCrystalGrowth;
-import androsa.gaiadimension.world.gen.GDGenNoTrees;
-import androsa.gaiadimension.world.gen.GDGenPurpleAgateTree;
+import androsa.gaiadimension.world.gen.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -19,13 +19,14 @@ import java.util.Random;
 public class GDPurpleAgateSwamp extends GDBiomeBase {
 
     private WorldGenTrees GaiaGenPurpleTrees;
+    private GDGenGummyBlob genGummyBlock = new GDGenGummyBlob(GDBlocks.gummyGlitterBlock, 0);
     private short[] skyColorRGB = new short[] { 171, 109, 241 };
 
     public GDPurpleAgateSwamp(BiomeProperties props) {
         super(props);
 
-        spawnableCreatureList.add(new SpawnListEntry(androsa.gaiadimension.entity.GDMystifiedGrowthSapper.class, 9, 3, 5));
-        spawnableCreatureList.add(new SpawnListEntry(androsa.gaiadimension.entity.GDSpellElement.class, 5, 1, 2));
+        spawnableCreatureList.add(new SpawnListEntry(androsa.gaiadimension.entity.GDMystifiedGrowthSapper.class, 10, 3, 5));
+        spawnableCreatureList.add(new SpawnListEntry(androsa.gaiadimension.entity.GDSpellElement.class, 10, 1, 2));
 
         //TODO: Decorate with unique plants/mobs to stand out more as a swamp
 
@@ -35,6 +36,20 @@ public class GDPurpleAgateSwamp extends GDBiomeBase {
 
         topBlock = GDBlocks.scentedGrass.getDefaultState();
         fillerBlock = GDBlocks.heavySoil.getDefaultState();
+    }
+
+    @Override
+    public void decorate(World world, Random rand, BlockPos pos) {
+        int dx, dy, dz;
+
+        int maxBoulder = rand.nextInt(2);
+        for (int i = 0; i < maxBoulder; ++i) {
+            dx = pos.getX() + rand.nextInt(16) + 8;
+            dz = pos.getZ() + rand.nextInt(16) + 8;
+            genGummyBlock.generate(world, rand, world.getHeight(new BlockPos(dx, 0, dz)));
+        }
+
+        super.decorate(world, rand, pos);
     }
 
     @SideOnly(Side.CLIENT)
