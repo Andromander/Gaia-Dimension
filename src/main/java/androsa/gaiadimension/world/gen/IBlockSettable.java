90bc1db047e5896951307545e1e3bc275a0cc22e
Andromander <cathy@feodoroff.com>
2018-07-13 10:37:57 +1000

    Remove these two useless files

--- src/main/java/androsa/gaiadimension/world/gen/IBlockSettable.java
+++
@@ -1,9 +0,0 @@
-package androsa.gaiadimension.world.gen;
-
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public interface IBlockSettable {
-    void setBlockAndNotify(World world, BlockPos pos, IBlockState state);
-}
