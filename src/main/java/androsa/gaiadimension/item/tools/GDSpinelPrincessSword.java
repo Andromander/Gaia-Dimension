079eceecb49c178181444255397aa904869b4fd9
Andromander <cathy@feodoroff.com>
2018-08-04 20:45:00 +1000

    Spinel Princess Flamberge does fire stuff

--- src/main/java/androsa/gaiadimension/item/tools/GDSpinelPrincessSword.java
+++ src/main/java/androsa/gaiadimension/item/tools/GDSpinelPrincessSword.java
@@ -5,13 +5,16 @@ import androsa.gaiadimension.registry.ModelRegisterCallback;
 import com.google.common.collect.Multimap;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -40,9 +43,23 @@ public class GDSpinelPrincessSword extends ItemSword implements ModelRegisterCal
         tooltips.add(I18n.format(getUnlocalizedName() + ".tooltip"));
     }
 
-    //TODO: Deals fire damage to hit target
-    //TODO: [FUTURE] Can be used to unlock Unknown Zircon Prince Structure
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+        if (player.world.isRemote) {
+            for (int var1 = 0; var1 < 20; ++var1) {
+                double px = entity.posX + itemRand.nextFloat() * entity.width;
+                double py = entity.posY + itemRand.nextFloat() * entity.height;
+                double pz = entity.posZ + itemRand.nextFloat() * entity.width;
+                entity.world.spawnParticle(EnumParticleTypes.FLAME, px, py, pz, 1, 1, 1);
+            }
+        }
 
+        entity.setFire(10);
+
+        return false;
+    }
+
+    //TODO: [FUTURE] Can be used to unlock Unknown Zircon Prince Structure
 
     //We will need this in order to make the weapon slow
     @Override
