886ac52514ae36d3bd1b8048fa836560940ac267
Andromander <cathy@feodoroff.com>
2018-10-16 22:39:07 +1000

    The most general blocks have been
    crumch

---
+++ src/main/java/androsa/gaiadimension/block/GDBlock.java
@@ -0,0 +1,28 @@
+package androsa.gaiadimension.block;
+
+import androsa.gaiadimension.registry.GDTabs;
+import androsa.gaiadimension.registry.ModelRegisterCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+/**
+ * The generic Block class in the event of a block that does not have special properties.
+ */
+public class GDBlock extends Block implements ModelRegisterCallback {
+
+    public GDBlock(Material material, MapColor color, SoundType sound) {
+        super(material, color);
+
+        setSoundType(sound);
+        setCreativeTab(GDTabs.tabBlock);
+    }
+
+    public GDBlock(Material material, MapColor color) {
+        super(material, color);
+
+        setSoundType(SoundType.STONE);
+        setCreativeTab(GDTabs.tabBlock);
+    }
+}
--- src/main/java/androsa/gaiadimension/block/GDOre.java
+++ src/main/java/androsa/gaiadimension/block/GDOre.java
@@ -1,32 +1,38 @@
-package androsa.gaiadimension.block.blocksore;
+package androsa.gaiadimension.block;
 
 import androsa.gaiadimension.registry.GDTabs;
 import androsa.gaiadimension.registry.ModelRegisterCallback;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 import java.util.function.Supplier;
 
-public class GDOreOpal extends Block implements ModelRegisterCallback {
+public class GDOre extends Block implements ModelRegisterCallback {
 
     private final Supplier<Item> itemSupplier;
 
-    public GDOreOpal(Supplier<Item> item) {
-        super(Material.ROCK);
+    public GDOre(Material material, MapColor color, @Nullable Supplier<Item> item) {
+        super(material, color);
 
-        this.setHardness(2.0F);
-        this.setResistance(15.0F);
-        this.setCreativeTab(GDTabs.tabBlock);
+        setSoundType(SoundType.STONE);
+        setCreativeTab(GDTabs.tabBlock);
 
         itemSupplier = item;
     }
 
     @Override
     public Item getItemDropped(IBlockState state, Random random, int j) {
-        return itemSupplier.get();
+        if (itemSupplier != null) {
+            return itemSupplier.get();
+        } else {
+            return Item.getItemFromBlock(this);
+        }
     }
 
     @Override
