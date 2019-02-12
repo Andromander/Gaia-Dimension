f5384cc85f67ad8991e9658ff15904dbeae39b7e
Andromander <cathy@feodoroff.com>
2019-02-12 20:21:29 +1000

    Fix Spellbound Elemental not spawning

--- src/main/java/androsa/gaiadimension/entity/GDSpellElement.java
+++ src/main/java/androsa/gaiadimension/entity/GDSpellElement.java
@@ -28,7 +28,6 @@ public class GDSpellElement extends EntityCreature {
     protected final void applyEntityAttributes() {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(15.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3);
     }
 
     @Override
@@ -38,7 +37,6 @@ public class GDSpellElement extends EntityCreature {
         this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(4, new EntityAILookIdle(this));
         this.tasks.addTask(2, new EntityAIWander(this, 0.5D));
-        //this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[0]));
     }
 
     @Override
