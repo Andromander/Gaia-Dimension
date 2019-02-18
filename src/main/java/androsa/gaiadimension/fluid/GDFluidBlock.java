f0ce86e41c01ab0caaf4ac57e5fec712296c75c4
Andromander <cathy@feodoroff.com>
2019-02-18 22:53:38 +1000

    Improved Fluid logic
    Now they all mix

--- src/main/java/androsa/gaiadimension/fluid/GDFluidBlock.java
+++ src/main/java/androsa/gaiadimension/fluid/GDFluidBlock.java
@@ -1,24 +1,28 @@
 package androsa.gaiadimension.fluid;
 
+import androsa.gaiadimension.registry.GDBlocks;
 import androsa.gaiadimension.registry.GDTabs;
 import androsa.gaiadimension.registry.ModelRegisterCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
 
 public class GDFluidBlock extends BlockFluidClassic implements ModelRegisterCallback {
 
@@ -26,6 +30,46 @@ public class GDFluidBlock extends BlockFluidClassic implements ModelRegisterCall
         super(fluid, material);
         setLightOpacity(4);
         setCreativeTab(GDTabs.tabBlock);
+    }
+
+    @Override
+    public void onBlockAdded(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+        super.onBlockAdded(world, pos, state);
+
+        this.mixFluids(world, pos);
+    }
+
+    @Override
+    public void neighborChanged(@Nonnull IBlockState state, @Nonnull World world, @Nonnull BlockPos pos, @Nonnull Block neighborBlock, @Nonnull BlockPos neighbourPos) {
+        super.neighborChanged(state, world, pos, neighborBlock, neighbourPos);
+
+        this.mixFluids(world, pos);
+    }
+
+    public void mixFluids(World world, BlockPos pos) {
+        for (EnumFacing side : EnumFacing.VALUES) {
+            if (side != EnumFacing.DOWN) {
+                IBlockState offset = world.getBlockState(pos.offset(side));
+                if (offset.getMaterial().isLiquid()) {
+                     if ((offset.getMaterial() == Material.LAVA) && (offset.getBlock() != GDBlocks.superhot_magma_block) && (offset.getBlock() instanceof BlockFluidBase || offset.getBlock() instanceof BlockLiquid)) {
+                        world.setBlockState(pos, GDBlocks.gaia_cobblestone.getDefaultState());
+                        this.playSound(world, pos);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    protected void playSound(World worldIn, BlockPos pos) {
+        double d0 = (double)pos.getX();
+        double d1 = (double)pos.getY();
+        double d2 = (double)pos.getZ();
+        worldIn.playSound(null, pos, SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (worldIn.rand.nextFloat() - worldIn.rand.nextFloat()) * 0.8F);
+
+        for (int i = 0; i < 8; ++i) {
+            worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0 + Math.random(), d1 + 1.2D, d2 + Math.random(), 0.0D, 0.0D, 0.0D);
+        }
     }
 
     @Override
--- src/main/java/androsa/gaiadimension/fluid/GDSuperhotMagma.java
+++ src/main/java/androsa/gaiadimension/fluid/GDSuperhotMagma.java
@@ -1,26 +1,100 @@
 package androsa.gaiadimension.fluid;
 
+import androsa.gaiadimension.registry.GDBlocks;
 import androsa.gaiadimension.registry.ModelRegisterCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 
-public class GDSuperhotMagma extends BlockFluidClassic implements ModelRegisterCallback {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class GDSuperhotMagma extends GDFluidBlock implements ModelRegisterCallback {
 
     public GDSuperhotMagma(Fluid fluid, Material material) {
         super(fluid, material);
         setLightLevel(1.0F);
         setLightOpacity(0);
+    }
+
+    @Override
+    public void onBlockAdded(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+        super.onBlockAdded(world, pos, state);
+
+        this.mixFluids(world, pos);
+    }
+
+    @Override
+    public void neighborChanged(@Nonnull IBlockState state, @Nonnull World world, @Nonnull BlockPos pos, @Nonnull Block neighborBlock, @Nonnull BlockPos neighbourPos) {
+        super.neighborChanged(state, world, pos, neighborBlock, neighbourPos);
+
+        this.mixFluids(world, pos);
+    }
+
+    @Override
+    public void mixFluids(World world, BlockPos pos) {
+        for (EnumFacing side : EnumFacing.VALUES) {
+            if (side != EnumFacing.DOWN) {
+                IBlockState offset = world.getBlockState(pos.offset(side));
+                if (offset.getMaterial().isLiquid()) {
+                    if (offset.getBlock() instanceof GDFluidBlock) {
+                        if (offset.getBlock() == GDBlocks.mineral_water_block) {
+                            world.setBlockState(pos, GDBlocks.gaia_stone.getDefaultState());
+                            this.playSound(world, pos);
+                            break;
+                        }
+                        if (offset.getBlock() == GDBlocks.sweet_muck_block) {
+                            world.setBlockState(pos, GDBlocks.primal_mass.getDefaultState());
+                            this.playSound(world, pos);
+                            break;
+                        }
+                    } else if (offset.getBlock() instanceof BlockFluidBase || offset.getBlock() instanceof BlockLiquid) {
+                        if (offset.getMaterial() == Material.WATER) {
+                            world.setBlockState(pos, GDBlocks.gaia_cobblestone.getDefaultState());
+                            this.playSound(world, pos);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    protected void playSound(World worldIn, BlockPos pos) {
+        double d0 = (double)pos.getX();
+        double d1 = (double)pos.getY();
+        double d2 = (double)pos.getZ();
+        worldIn.playSound(null, pos, SoundEvents.BLOCK_LAVA_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (worldIn.rand.nextFloat() - worldIn.rand.nextFloat()) * 0.8F);
+
+        for (int i = 0; i < 8; ++i) {
+            worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0 + Math.random(), d1 + 1.2D, d2 + Math.random(), 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    @Override
+    public void onEntityCollision(World world, BlockPos pos, IBlockState state, Entity entity) {
+        super.onEntityCollision(world, pos, state, entity);
+        if(!entity.isImmuneToFire()) {
+            entity.attackEntityFrom(DamageSource.IN_FIRE, 5.0F);
+        }
     }
 
     @Override
@@ -36,13 +110,5 @@ public class GDSuperhotMagma extends BlockFluidClassic implements ModelRegisterC
                 return modelResourceLocation;
             }
         });
-    }
-
-    @Override
-    public void onEntityCollision(World world, BlockPos pos, IBlockState state, Entity entity) {
-        super.onEntityCollision(world, pos, state, entity);
-        if(!entity.isImmuneToFire()) {
-            entity.attackEntityFrom(DamageSource.IN_FIRE, 5.0F);
-        }
     }
 }
