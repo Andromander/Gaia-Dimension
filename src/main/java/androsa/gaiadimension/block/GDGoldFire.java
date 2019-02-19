fa44ed0b1ab4f349b984f8234d7aa1e5257013a3
Andromander <cathy@feodoroff.com>
2019-02-19 10:14:03 +1000

    Try this fix for Gold Fire
    Can't seem to replicate it. Maybe this fixed it?

--- src/main/java/androsa/gaiadimension/block/GDGoldFire.java
+++ src/main/java/androsa/gaiadimension/block/GDGoldFire.java
@@ -1,51 +1,54 @@
 package androsa.gaiadimension.block;
 
 import androsa.gaiadimension.registry.GDBlocks;
-import androsa.gaiadimension.registry.GDTabs;
 import androsa.gaiadimension.registry.ModelRegisterCallback;
+import androsa.gaiadimension.registry.ModelUtils;
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Random;
 
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class GDGoldFire extends Block implements ModelRegisterCallback {
+    public static final PropertyInteger AGE = PropertyInteger.create("age", 0, 15);
 
     public GDGoldFire() {
-        super(Material.FIRE);
+        super(Material.FIRE, MapColor.GOLD);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(AGE, 0));
         this.setTickRandomly(true);
-        this.setLightLevel(1.0F);
-    }
-
-    @Override
-    public boolean isBurning(IBlockAccess world, BlockPos pos) {
-        return true;
     }
 
     @Override
     @Deprecated
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess world, BlockPos pos) {
-        return null;
-    }
-
-    @Override
-    @Deprecated
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block neighborBlock, BlockPos pos2) {
-        if(!worldIn.getBlockState(pos.down()).isSideSolid(worldIn, pos.down(), EnumFacing.UP))
-            worldIn.setBlockToAir(pos);
+    @Nullable
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return NULL_AABB;
     }
 
     @Override
@@ -65,9 +68,45 @@ public class GDGoldFire extends Block implements ModelRegisterCallback {
         return 0;
     }
 
-    @Override
+    /**
+     * How many world ticks before ticking
+     */
     public int tickRate(World worldIn) {
         return 30;
+    }
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        if (worldIn.getGameRules().getBoolean("doFireTick")) {
+            if (!worldIn.isAreaLoaded(pos, 2)) return;
+            if (!this.canPlaceBlockAt(worldIn, pos)) {
+                worldIn.setBlockToAir(pos);
+            }
+
+            int age = state.getValue(AGE);
+
+            if (worldIn.isRaining() && this.canDie(worldIn, pos) && rand.nextFloat() < 0.2F + (float)age * 0.03F) {
+                worldIn.setBlockToAir(pos);
+            } else {
+                if (age < 15) {
+                    state = state.withProperty(AGE, age + (rand.nextInt(3) / 2));
+                    worldIn.setBlockState(pos, state, 4);
+                }
+
+                worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn) + rand.nextInt(10));
+            }
+        }
+    }
+
+    protected boolean canDie(World worldIn, BlockPos pos) {
+        return worldIn.isRainingAt(pos) || worldIn.isRainingAt(pos.west()) || worldIn.isRainingAt(pos.east()) || worldIn.isRainingAt(pos.north()) || worldIn.isRainingAt(pos.south());
+    }
+
+    @Override
+    public void onEntityCollision(World par1World, BlockPos pos, IBlockState state, Entity entity) {
+        super.onEntityCollision(par1World, pos, state, entity);
+
+        if(entity instanceof EntityLivingBase)
+            ((EntityLivingBase)entity).addPotionEffect(new PotionEffect(MobEffects.GLOWING, 100));
     }
 
     @Override
@@ -81,17 +120,16 @@ public class GDGoldFire extends Block implements ModelRegisterCallback {
     }
 
     @Override
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.CUTOUT;
+    public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+        return worldIn.getBlockState(pos.down()).isTopSolid();
     }
 
     @Override
-    public void onEntityCollision(World par1World, BlockPos pos, IBlockState state, Entity par5Entity) {
-        super.onEntityCollision(par1World, pos, state, par5Entity);
-
-        if(par5Entity instanceof EntityLivingBase)
-            ((EntityLivingBase)par5Entity).addPotionEffect(new PotionEffect(MobEffects.GLOWING, 100));
+    @Deprecated
+    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+        if (!worldIn.getBlockState(pos.down()).isTopSolid()) {
+            worldIn.setBlockToAir(pos);
+        }
     }
 
     @Override
@@ -105,5 +143,55 @@ public class GDGoldFire extends Block implements ModelRegisterCallback {
         } else {
             world.scheduleUpdate(pos, this, this.tickRate(world) + world.rand.nextInt(10));
         }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        if (rand.nextInt(24) == 0) {
+            worldIn.playSound((double)((float)pos.getX() + 0.5F), (double)((float)pos.getY() + 0.5F), (double)((float)pos.getZ() + 0.5F), SoundEvents.BLOCK_FIRE_AMBIENT, SoundCategory.BLOCKS, 1.0F + rand.nextFloat(), rand.nextFloat() * 0.7F + 0.3F, false);
+        } else {
+            for (int i = 0; i < 3; ++i)
+            {
+                double d0 = (double)pos.getX() + rand.nextDouble();
+                double d1 = (double)pos.getY() + rand.nextDouble() * 0.5D + 0.5D;
+                double d2 = (double)pos.getZ() + rand.nextDouble();
+                worldIn.spawnParticle(EnumParticleTypes.CRIT, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            }
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(AGE, meta);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(AGE);
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, AGE);
+    }
+
+    @Override
+    @Deprecated
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerModel() {
+        ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(AGE).build());
+        ModelUtils.registerToState(this, 0, getDefaultState());
     }
 }
