77f26f24e91373fe1a89b01d36bba19f09300c22
Andromander <cathy@feodoroff.com>
2019-03-24 08:30:38 +1000

    Hotfix Scaynyx Bucket

--- src/main/java/androsa/gaiadimension/GaiaDimension.java
+++ src/main/java/androsa/gaiadimension/GaiaDimension.java
@@ -32,7 +32,7 @@ import org.apache.logging.log4j.Logger;
 
 public class GaiaDimension {
     public static final String MODID = "gaiadimension";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String NAME = "Gaia Dimension";
 
     public static final GDItems items = new GDItems();
