afbb706f1f9a3e29544d760ed2b4013fce3f02fa
Andromander <cathy@feodoroff.com>
2019-06-03 11:45:17 +1000

    Fix #43

--- src/main/java/androsa/gaiadimension/block/GDAgatePlankSlab.java
+++ src/main/java/androsa/gaiadimension/block/GDAgatePlankSlab.java
@@ -1,114 +1,19 @@
 package androsa.gaiadimension.block;
 
-import androsa.gaiadimension.registry.GDTabs;
-import androsa.gaiadimension.registry.ModelRegisterCallback;
-import androsa.gaiadimension.registry.ModelUtils;
-import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.block.BlockSlab;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.statemap.StateMap;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.item.Item;
 
-import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Locale;
+import java.util.function.Supplier;
 
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-public class GDAgatePlankSlab extends BlockSlab implements ModelRegisterCallback {
+public class GDAgatePlankSlab extends GDGaiaSlab {
 
-    private static final PropertyEnum<AgatePlankSlab> VARIANT = PropertyEnum.create("variant", AgatePlankSlab.class);
-    private final boolean isDouble;
-    private final MapColor mapColor;
+    public GDAgatePlankSlab(boolean isDouble, MapColor color, Supplier<Item> drop) {
+        super(isDouble, Material.WOOD, color, SoundType.STONE, "axe", 0, drop);
 
-    public GDAgatePlankSlab(boolean isDouble, MapColor color) {
-        super(Material.WOOD, color);
-
-        this.setSoundType(SoundType.STONE);
-        this.setCreativeTab(GDTabs.tabBlock);
         this.setHardness(1.5F);
         this.setResistance(2.0F);
-        this.setLightOpacity(isDouble ? 255 : 0);
-        this.setHarvestLevel("axe", 0);
-        this.isDouble = isDouble;
-        this.mapColor = color;
-
-        IBlockState state = this.blockState.getBaseState().withProperty(VARIANT, AgatePlankSlab.NORMAL);
-        if (!this.isDouble())
-            state = state.withProperty(HALF, EnumBlockHalf.BOTTOM);
-        this.setDefaultState(state);
-    }
-
-    @Override
-    @Deprecated
-    public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        return mapColor;
-    }
-
-    @Override
-    public IProperty<?> getVariantProperty() {
-        return VARIANT;
-    }
-
-    @Override
-    public Comparable<?> getTypeForItem(ItemStack stack) {
-        return AgatePlankSlab.NORMAL;
-    }
-
-    @Override
-    public boolean isDouble() {
-        return isDouble;
-    }
-
-    @Override
-    public String getTranslationKey(int meta) {
-        return super.getTranslationKey();
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return this.isDouble() ? new BlockStateContainer(this, VARIANT) : new BlockStateContainer(this, VARIANT, HALF);
-    }
-
-    @Override
-    @Deprecated
-    public IBlockState getStateFromMeta(int meta) {
-        return this.isDouble() ? this.getDefaultState() : this.getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(HALF).ordinal();
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerModel() {
-        if (this.isDouble())
-            ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(VARIANT).ignore(HALF).build());
-        else {
-            ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(VARIANT).build());
-            ModelUtils.registerToState(this, 0, getDefaultState());
-        }
-    }
-
-    public enum AgatePlankSlab implements IStringSerializable {
-        NORMAL;
-
-        public String getName() {
-            return name().toLowerCase(Locale.ROOT);
-        }
     }
 }
 
--- src/main/java/androsa/gaiadimension/block/GDMalachiteBrickSlab.java
+++ src/main/java/androsa/gaiadimension/block/GDMalachiteBrickSlab.java
@@ -1,110 +1,18 @@
 package androsa.gaiadimension.block;
 
-import androsa.gaiadimension.registry.GDBlocks;
-import androsa.gaiadimension.registry.GDTabs;
-import androsa.gaiadimension.registry.ModelRegisterCallback;
-import androsa.gaiadimension.registry.ModelUtils;
-import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.block.BlockSlab;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IStringSerializable;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Locale;
-import java.util.Random;
+import java.util.function.Supplier;
 
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-public class GDMalachiteBrickSlab extends BlockSlab implements ModelRegisterCallback {
+public class GDMalachiteBrickSlab extends GDGaiaSlab {
 
-    private static final PropertyEnum<MalachiteSlabVariant> VARIANT = PropertyEnum.create("variant", MalachiteSlabVariant.class);
+    public GDMalachiteBrickSlab(boolean isDouble, Supplier<Item> drop) {
+        super(isDouble, Material.ROCK, MapColor.GREEN, SoundType.STONE, "pickaxe", 2, drop);
 
-    private final boolean isDouble;
-
-    public GDMalachiteBrickSlab(boolean isDouble) {
-        super(Material.ROCK);
-
-        this.isDouble = isDouble;
-        this.setCreativeTab(GDTabs.tabBlock);
         this.setHardness(20F);
         this.setResistance(100F);
-        this.setLightOpacity(isDouble ? 255 : 0);
-        this.setHarvestLevel("pickaxe", 2);
-
-        IBlockState state = this.blockState.getBaseState().withProperty(VARIANT, MalachiteSlabVariant.NORMAL);
-
-        if (!this.isDouble()) state = state.withProperty(HALF, EnumBlockHalf.BOTTOM);
-
-        this.setDefaultState(state);
-    }
-
-    @Override
-    public IProperty<?> getVariantProperty() {
-        return VARIANT;
-    }
-
-    @Override
-    public Comparable<?> getTypeForItem(ItemStack stack) {
-        return MalachiteSlabVariant.NORMAL;
-    }
-
-    @Override
-    public boolean isDouble() {
-        return isDouble;
-    }
-
-    @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return Item.getItemFromBlock(GDBlocks.malachite_brick_slab);
-    }
-
-    @Override
-    public String getTranslationKey(int meta) {
-        return super.getTranslationKey();
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return this.isDouble() ? new BlockStateContainer(this, VARIANT) : new BlockStateContainer(this, VARIANT, HALF);
-    }
-
-    @Override
-    @Deprecated
-    public IBlockState getStateFromMeta(int meta) {
-        return this.isDouble() ? this.getDefaultState() : this.getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(HALF).ordinal();
-    }
-
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerModel() {
-        if (this.isDouble())
-            ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(VARIANT).ignore(HALF).build());
-        else {
-            ModelLoader.setCustomStateMapper(this, new StateMap.Builder().ignore(VARIANT).build());
-            ModelUtils.registerToState(this, 0, getDefaultState());
-        }
-    }
-
-    public enum MalachiteSlabVariant implements IStringSerializable {
-        NORMAL;
-
-        public String getName() {
-            return name().toLowerCase(Locale.ROOT);
-        }
     }
 }
