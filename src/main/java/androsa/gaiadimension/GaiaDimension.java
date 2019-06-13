2e1dad80f8e1bd04636908aba733e7c2085b552b
Andromander <cathy@feodoroff.com>
2019-06-13 19:10:15 +1000

    Version Bump

--- src/main/java/androsa/gaiadimension/GaiaDimension.java
+++ src/main/java/androsa/gaiadimension/GaiaDimension.java
@@ -33,7 +33,7 @@ import org.apache.logging.log4j.Logger;
 
 public class GaiaDimension {
     public static final String MODID = "gaiadimension";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
     public static final String NAME = "Gaia Dimension";
 
     public static final GDItems items = new GDItems();
