a8502e5081b0f411ba22427306a4ca69f456a0ee
Andromander <cathy@feodoroff.com>
2019-06-30 20:52:01 +1000

    Prepare the new environment

--- src/main/java/androsa/gaiadimension/GaiaDimension.java
+++ src/main/java/androsa/gaiadimension/GaiaDimension.java
@@ -1,44 +1,27 @@
 package androsa.gaiadimension;
 
 import androsa.gaiadimension.proxy.CommonProxy;
-import androsa.gaiadimension.registry.GDBlocks;
 import androsa.gaiadimension.registry.GDConfig;
-import androsa.gaiadimension.registry.GDFluids;
-import androsa.gaiadimension.registry.GDItems;
 import androsa.gaiadimension.world.WorldProviderGaia;
-import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.DimensionType;
+import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = GaiaDimension.MODID,
-        name = "GaiaDimension",
-        version = GaiaDimension.VERSION,
-        dependencies = "required-after:forge@[14.23.5.2768,)",
-        updateJSON = "https://raw.githubusercontent.com/Andromander/Gaia-Dimension/master/update.json"
-)
-
+// updateJSON = "https://raw.githubusercontent.com/Andromander/Gaia-Dimension/master/update.json"
+@Mod(GaiaDimension.MODID)
 public class GaiaDimension {
     public static final String MODID = "gaiadimension";
     public static final String VERSION = "1.0.5";
     public static final String NAME = "Gaia Dimension";
 
-    public static final GDItems items = new GDItems();
-    public static final GDBlocks blocks = new GDBlocks();
-    public static final GDFluids fluids = new GDFluids();
     public static final String ARMOR_DIR = "gaiadimension:textures/armor/";
     public static final String MODEL_DIR = "gaiadimension:textures/model/";
     public static final ResourceLocation POTION_TEXTURES = new ResourceLocation(MODID, "textures/gui/potions.png");
@@ -46,14 +29,12 @@ public class GaiaDimension {
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     public static DimensionType dimType;
-    public static int backupdimensionID = -258;
 
-    public static final EnumCreatureAttribute GAIAN = EnumHelper.addCreatureAttribute("GAIAN");
-    public static final EnumCreatureAttribute CORRUPT = EnumHelper.addCreatureAttribute("CORRUPT");
+    public static final CreatureAttribute GAIAN = new CreatureAttribute();
+    public static final CreatureAttribute CORRUPT = new CreatureAttribute();
 
     public static final DamageSource CORRUPTION = new DamageSource("corruption").setDamageBypassesArmor();
 
-    @Instance(MODID)
     public static GaiaDimension instance;
 
     @SidedProxy(clientSide = "androsa.gaiadimension.proxy.ClientProxy", serverSide = "androsa.gaiadimension.proxy.CommonProxy")
@@ -92,12 +73,6 @@ public class GaiaDimension {
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        if (!DimensionManager.isDimensionRegistered(GDConfig.dimension.dimensionID)) {
-            DimensionManager.registerDimension(GDConfig.dimension.dimensionID, GaiaDimension.dimType);
-        } else {
-            GaiaDimension.LOGGER.warn("The ID '{}' chosen in the config log is already in use. Falling back onto backup dimension ID");
-            DimensionManager.registerDimension(GaiaDimension.backupdimensionID, GaiaDimension.dimType);
-            GDConfig.dimension.dimensionID = GaiaDimension.backupdimensionID;
-        }
+        DimensionManager.registerDimension(GDConfig.dimension.dimensionID, GaiaDimension.dimType);
     }
 }
