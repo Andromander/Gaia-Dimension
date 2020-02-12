c7b8d93926a0f24f27e2db225ff9ce0f357c6b85
Andromander <cathy@feodoroff.com>
2020-02-12 14:16:50 +1000

    Fix a few loot tables, they weren't correct

--- src/main/java/androsa/gaiadimension/data/provider/GaiaBlockLootTableProvider.java
+++ src/main/java/androsa/gaiadimension/data/provider/GaiaBlockLootTableProvider.java
@@ -18,16 +18,16 @@ import net.minecraft.world.storage.loot.conditions.TableBonus;
 import net.minecraft.world.storage.loot.functions.CopyName;
 import net.minecraft.world.storage.loot.functions.CopyNbt;
 import net.minecraft.world.storage.loot.functions.SetContents;
+import net.minecraft.world.storage.loot.functions.SetCount;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public abstract class GaiaBlockLootTableProvider extends BlockLootTables {
-
-    private static final ILootCondition.IBuilder silk_touch_condition = MatchTool.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.IntBound.atLeast(1))));
-    private static final ILootCondition.IBuilder shears_condition = MatchTool.builder(ItemPredicate.Builder.create().item(Items.SHEARS));
-    private static final ILootCondition.IBuilder alt_condition = shears_condition.alternative(silk_touch_condition);
-    private static final ILootCondition.IBuilder inv_condition = alt_condition.inverted();
+    private static final ILootCondition.IBuilder has_silk_touch = MatchTool.builder(ItemPredicate.Builder.create().enchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.IntBound.atLeast(1))));
+    private static final ILootCondition.IBuilder has_shears = MatchTool.builder(ItemPredicate.Builder.create().item(Items.SHEARS));
+    private static final ILootCondition.IBuilder shears_or_silk = has_shears.alternative(has_silk_touch);
+    private static final ILootCondition.IBuilder silk_or_shears = shears_or_silk.inverted();
 
     public void registerTable(Supplier<? extends Block> block, Function<Block, LootTable.Builder> factory) {
         super.registerLootTable(block.get(), factory);
@@ -61,23 +61,67 @@ public abstract class GaiaBlockLootTableProvider extends BlockLootTables {
         registerLootTable(block.get(), (result) -> withChance(block.get(), drop.get(), chances));
     }
 
+    public void dropChanceAlternative(Supplier<? extends Block> block, Supplier<? extends Block> drop, Supplier<? extends Item> item, float... chances) {
+        registerLootTable(block.get(), (result) -> withChanceAdditional(block.get(), drop.get(), item.get(), chances));
+    }
+
+    public void dropChanceAlternative(Supplier<? extends Block> block, Supplier<? extends Block> drop, Item item, float... chances) {
+        registerLootTable(block.get(), (result) -> withChanceAdditional(block.get(), drop.get(), item, chances));
+    }
+
     public void dropAlternative(Supplier<? extends Block> block, Supplier<? extends IItemProvider> drop) {
         registerLootTable(block.get(), (result) -> droppingWithSilkTouch(result, withSurvivesExplosion(result, ItemLootEntry.builder(drop.get()).acceptCondition(TableBonus.builder(Enchantments.FORTUNE, 0.1F, 0.14285715F, 0.25F, 1.0F)).alternatively(ItemLootEntry.builder(result)))));
     }
 
     protected static LootTable.Builder smallCrate(Block block) {
-        return LootTable.builder().addLootPool(withSurvivesExplosion(block, LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(block).acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY)).acceptFunction(CopyNbt.func_215881_a(CopyNbt.Source.BLOCK_ENTITY).func_216056_a("Lock", "BlockEntityTag.Lock").func_216056_a("LootTable", "BlockEntityTag.LootTable").func_216056_a("LootTableSeed", "BlockEntityTag.LootTableSeed")).acceptFunction(SetContents.func_215920_b().func_216075_a(DynamicLootEntry.func_216162_a(SmallCrateBlock.NAME))))));
+        return LootTable.builder()
+                .addLootPool(withSurvivesExplosion(block, LootPool.builder()
+                        .rolls(ConstantRange.of(1))
+                        .addEntry(ItemLootEntry.builder(block)
+                                .acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY))
+                                .acceptFunction(CopyNbt.func_215881_a(CopyNbt.Source.BLOCK_ENTITY)
+                                        .func_216056_a("Lock", "BlockEntityTag.Lock")
+                                        .func_216056_a("LootTable", "BlockEntityTag.LootTable")
+                                        .func_216056_a("LootTableSeed", "BlockEntityTag.LootTableSeed"))
+                                .acceptFunction(SetContents.func_215920_b()
+                                        .func_216075_a(DynamicLootEntry.func_216162_a(SmallCrateBlock.NAME))))));
     }
 
     protected static LootTable.Builder largeCrate(Block block) {
-        return LootTable.builder().addLootPool(withSurvivesExplosion(block, LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(block).acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY)).acceptFunction(CopyNbt.func_215881_a(CopyNbt.Source.BLOCK_ENTITY).func_216056_a("Lock", "BlockEntityTag.Lock").func_216056_a("LootTable", "BlockEntityTag.LootTable").func_216056_a("LootTableSeed", "BlockEntityTag.LootTableSeed")).acceptFunction(SetContents.func_215920_b().func_216075_a(DynamicLootEntry.func_216162_a(LargeCrateBlock.NAME))))));
+        return LootTable.builder()
+                .addLootPool(withSurvivesExplosion(block, LootPool.builder()
+                        .rolls(ConstantRange.of(1))
+                        .addEntry(ItemLootEntry.builder(block)
+                                .acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY))
+                                .acceptFunction(CopyNbt.func_215881_a(CopyNbt.Source.BLOCK_ENTITY)
+                                        .func_216056_a("Lock", "BlockEntityTag.Lock")
+                                        .func_216056_a("LootTable", "BlockEntityTag.LootTable")
+                                        .func_216056_a("LootTableSeed", "BlockEntityTag.LootTableSeed"))
+                                .acceptFunction(SetContents.func_215920_b()
+                                        .func_216075_a(DynamicLootEntry.func_216162_a(LargeCrateBlock.NAME))))));
     }
 
     protected static LootTable.Builder withName(Block block) {
-        return LootTable.builder().addLootPool(withSurvivesExplosion(block, LootPool.builder().rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(block).acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY)))));
+        return LootTable.builder()
+                .addLootPool(withSurvivesExplosion(block, LootPool.builder()
+                        .rolls(ConstantRange.of(1))
+                        .addEntry(ItemLootEntry.builder(block)
+                                .acceptFunction(CopyName.builder(CopyName.Source.BLOCK_ENTITY)))));
     }
 
     protected static LootTable.Builder withChance(Block block, Block drop, float... chances) {
-        return droppingWithSilkTouchOrShears(block, withSurvivesExplosion(block, ItemLootEntry.builder(drop)).acceptCondition(TableBonus.builder(Enchantments.FORTUNE, chances)));
+        return droppingWithSilkTouchOrShears(block, withSurvivesExplosion(block, ItemLootEntry.builder(drop))
+                .acceptCondition(TableBonus.builder(Enchantments.FORTUNE, chances)));
     }
+
+    protected static LootTable.Builder withChanceAdditional(Block block, Block sapling, Item item, float... chances) {
+        return droppingWithSilkTouchOrShears(block, withSurvivesExplosion(block, ItemLootEntry.builder(sapling))
+                .acceptCondition(TableBonus.builder(Enchantments.FORTUNE, chances)))
+                .addLootPool(LootPool.builder().rolls(ConstantRange.of(1))
+                        .acceptCondition(silk_or_shears)
+                        .addEntry(withExplosionDecay(block, ItemLootEntry.builder(item)
+                                .acceptFunction(SetCount.builder(RandomValueRange.of(1.0F, 2.0F))))
+                                .acceptCondition(TableBonus.builder(Enchantments.FORTUNE, 0.02F, 0.022222223F, 0.025F, 0.033333335F, 0.1F))));
+    }
+
 }
