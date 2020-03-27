b8165626b419c21e0a5b82354e19c8dcc66617ae
Andromander <cathy@feodoroff.com>
2020-03-28 08:57:49 +1000

    New blocks

---
+++ src/main/java/androsa/gaiadimension/block/GaiaStairsBlock.java
@@ -0,0 +1,13 @@
+package androsa.gaiadimension.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.StairsBlock;
+
+import java.util.function.Supplier;
+
+public class GaiaStairsBlock extends StairsBlock {
+
+    public GaiaStairsBlock(Supplier<Block> state) {
+        super(() -> state.get().getDefaultState(), Properties.from(state.get()));
+    }
+}
