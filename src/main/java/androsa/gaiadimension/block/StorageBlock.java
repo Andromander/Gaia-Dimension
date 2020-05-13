9816e60f19cbd5d69f89b51c825482d1994601f1
Andromander <cathy@feodoroff.com>
2020-05-13 13:33:48 +1000

    Remove supports_beacon Tag. Back to method
    Saves on having to enforce minimum Forge version due to the tag being removed

--- src/main/java/androsa/gaiadimension/block/StorageBlock.java
+++ src/main/java/androsa/gaiadimension/block/StorageBlock.java
@@ -1,8 +1,11 @@
 package androsa.gaiadimension.block;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
 public class StorageBlock extends BasicGaiaBlock {
@@ -13,5 +16,10 @@ public class StorageBlock extends BasicGaiaBlock {
 
     public StorageBlock(MaterialColor color, int light) {
         super(Material.IRON, color, 5.0F, 10.0F, SoundType.METAL, ToolType.PICKAXE, 2, light);
+    }
+
+    @Override
+    public boolean isBeaconBase(BlockState state, IWorldReader world, BlockPos pos, BlockPos beacon) {
+        return true;
     }
 }
