fae999da0069b55e65c53823590b3ef4beda3268
Andromander <cathy@feodoroff.com>
2021-03-09 19:25:18 +1000

    Supply dummy Biomes

--- src/main/java/androsa/gaiadimension/registry/ModBiomes.java
+++ src/main/java/androsa/gaiadimension/registry/ModBiomes.java
@@ -1,15 +1,16 @@
 package androsa.gaiadimension.registry;
 
 import androsa.gaiadimension.GaiaDimensionMod;
-import androsa.gaiadimension.data.GaiaBiomes;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeAmbience;
+import net.minecraft.world.biome.BiomeGenerationSettings;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilders;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-
-import java.util.function.Supplier;
 
 import static net.minecraftforge.common.BiomeDictionary.*;
 
@@ -17,24 +18,36 @@ public class ModBiomes {
 
     public static final DeferredRegister<Biome> BIOMES = DeferredRegister.create(ForgeRegistries.BIOMES, GaiaDimensionMod.MODID);
 
-    public static final RegistryKey<Biome> pink_agate_forest = registerBiome("pink_agate_forest", GaiaBiomes::makePinkAgateForest);
-    public static final RegistryKey<Biome> blue_agate_taiga = registerBiome("blue_agate_taiga", GaiaBiomes::makeBlueAgateTaiga);
-    public static final RegistryKey<Biome> green_agate_jungle = registerBiome("green_agate_jungle", GaiaBiomes::makeGreenAgateJungle);
-    public static final RegistryKey<Biome> purple_agate_swamp = registerBiome("purple_agate_swamp", GaiaBiomes::makePurpleAgateSwamp);
-    public static final RegistryKey<Biome> fossil_woodland = registerBiome("fossil_woodland", GaiaBiomes::makeFossilWoodland);
-    public static final RegistryKey<Biome> mutant_agate_wildwood = registerBiome("mutant_agate_wildwood", GaiaBiomes::makeMutantAgateWildwood);
-    public static final RegistryKey<Biome> volcanic_lands = registerBiome("volcanic_lands", GaiaBiomes::makeVolcanicLands);
-    public static final RegistryKey<Biome> static_wasteland = registerBiome("static_wasteland", GaiaBiomes::makeStaticWasteland);
-    public static final RegistryKey<Biome> goldstone_lands = registerBiome("goldstone_lands", GaiaBiomes::makeGoldstoneLands);
-    public static final RegistryKey<Biome> crystal_plains = registerBiome("crystal_plains", GaiaBiomes::makeCrystalPlains);
-    public static final RegistryKey<Biome> salt_dunes = registerBiome("salt_dunes", GaiaBiomes::makeSaltDunes);
-    public static final RegistryKey<Biome> shining_grove = registerBiome("shining_grove", GaiaBiomes::makeShiningGrove);
-    public static final RegistryKey<Biome> smoldering_bog = registerBiome("smoldering_bog", GaiaBiomes::makeSmolderingBog);
-    public static final RegistryKey<Biome> mineral_reservoir = registerBiome("mineral_reservoir", GaiaBiomes::makeMineralReservoir);
-    public static final RegistryKey<Biome> mineral_river = registerBiome("mineral_river", GaiaBiomes::makeMineralRiver);
+    public static final RegistryKey<Biome> pink_agate_forest = registerBiome("pink_agate_forest");
+    public static final RegistryKey<Biome> blue_agate_taiga = registerBiome("blue_agate_taiga");
+    public static final RegistryKey<Biome> green_agate_jungle = registerBiome("green_agate_jungle");
+    public static final RegistryKey<Biome> purple_agate_swamp = registerBiome("purple_agate_swamp");
+    public static final RegistryKey<Biome> fossil_woodland = registerBiome("fossil_woodland");
+    public static final RegistryKey<Biome> mutant_agate_wildwood = registerBiome("mutant_agate_wildwood");
+    public static final RegistryKey<Biome> volcanic_lands = registerBiome("volcanic_lands");
+    public static final RegistryKey<Biome> static_wasteland = registerBiome("static_wasteland");
+    public static final RegistryKey<Biome> goldstone_lands = registerBiome("goldstone_lands");
+    public static final RegistryKey<Biome> crystal_plains = registerBiome("crystal_plains");
+    public static final RegistryKey<Biome> salt_dunes = registerBiome("salt_dunes");
+    public static final RegistryKey<Biome> shining_grove = registerBiome("shining_grove");
+    public static final RegistryKey<Biome> smoldering_bog = registerBiome("smoldering_bog");
+    public static final RegistryKey<Biome> mineral_reservoir = registerBiome("mineral_reservoir");
+    public static final RegistryKey<Biome> mineral_river = registerBiome("mineral_river");
 
-    private static RegistryKey<Biome> registerBiome(String name, Supplier<Biome> biomeSupplier) {
-        BIOMES.register(name, biomeSupplier);
+    private static RegistryKey<Biome> registerBiome(String name) {
+    	//Supply a dummy biome, as Biome are only required for IDs. They do not save generations
+        BIOMES.register(name, () -> new Biome.Builder()
+				.precipitation(Biome.RainType.NONE)
+				.category(Biome.Category.NONE)
+				.depth(0)
+				.downfall(0)
+				.scale(0)
+				.temperature(0)
+				.setEffects(new BiomeAmbience.Builder().setFogColor(0).setWaterColor(0).setWaterFogColor(0).withSkyColor(0).build())
+				.withGenerationSettings(new BiomeGenerationSettings.Builder().withSurfaceBuilder(ConfiguredSurfaceBuilders.field_244178_j).build())
+				.withMobSpawnSettings(new MobSpawnInfo.Builder().copy())
+				.withTemperatureModifier(Biome.TemperatureModifier.NONE)
+				.build());
         return RegistryKey.getOrCreateKey(Registry.BIOME_KEY, new ResourceLocation(GaiaDimensionMod.MODID, name));
     }
 
