c95f9e4e3ab09ed1b5cb08ef791d67117cc20491
Andromander <cathy@feodoroff.com>
2021-11-23 10:06:51 +1000

    Match terrain logic between the Towers

--- src/main/java/androsa/gaiadimension/world/gen/structure/MiniTowerStructure.java
+++ src/main/java/androsa/gaiadimension/world/gen/structure/MiniTowerStructure.java
@@ -15,7 +15,6 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
-import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 
@@ -65,25 +64,15 @@ public class MiniTowerStructure<T extends NoneFeatureConfiguration> extends Stru
             for(int x = mbb.minX(); x <= mbb.maxX(); ++x) {
                 for(int z = mbb.minZ(); z <= mbb.maxZ(); ++z) {
                     BlockPos blockpos = new BlockPos(x, minY, z);
-                    if (!world.isEmptyBlock(blockpos) && boundingbox.isInside(blockpos)) {
-                        boolean isAirBelow = false;
+                    if (!world.isEmptyBlock(blockpos) && boundingbox.isInside(blockpos) && this.isInsidePiece(blockpos)) {
 
-                        for(StructurePiece structurepiece : this.pieces) {
-                            if (structurepiece.getBoundingBox().isInside(blockpos)) {
-                                isAirBelow = true;
+                        for(int lowY = minY - 1; lowY > 1; --lowY) {
+                            BlockPos blockpos1 = new BlockPos(x, lowY, z);
+                            if (!world.isEmptyBlock(blockpos1) && !world.getBlockState(blockpos1).getMaterial().isLiquid()) {
                                 break;
                             }
-                        }
 
-                        if (isAirBelow) {
-                            for(int lowY = minY - 1; lowY > 1; --lowY) {
-                                BlockPos blockpos1 = new BlockPos(x, lowY, z);
-                                if (!world.isEmptyBlock(blockpos1) && !world.getBlockState(blockpos1).getMaterial().isLiquid()) {
-                                    break;
-                                }
-
-                                world.setBlock(blockpos1, world.getBiome(blockpos1).getGenerationSettings().getSurfaceBuilderConfig().getUnderMaterial(), 2);
-                            }
+                            world.setBlock(blockpos1, world.getBiome(blockpos1).getGenerationSettings().getSurfaceBuilderConfig().getUnderMaterial(), 2);
                         }
                     }
                 }
