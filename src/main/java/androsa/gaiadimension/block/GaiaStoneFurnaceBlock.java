41ce7b1fee3ebfe19d6a4cecbe0551d7f3866ca0
Andromander <androsa@protonmail.com>
2023-04-25 16:38:01 +1000

    Fix Gaia Stone Furnace not functionig

--- src/main/java/androsa/gaiadimension/block/GaiaStoneFurnaceBlock.java
+++ src/main/java/androsa/gaiadimension/block/GaiaStoneFurnaceBlock.java
@@ -1,13 +1,17 @@
 package androsa.gaiadimension.block;
 
 import androsa.gaiadimension.block.blockentity.GaiaStoneFurnaceBlockEntity;
+import androsa.gaiadimension.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FurnaceBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 
 public class GaiaStoneFurnaceBlock extends FurnaceBlock {
 
@@ -18,6 +22,12 @@ public class GaiaStoneFurnaceBlock extends FurnaceBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new GaiaStoneFurnaceBlockEntity(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> entity) {
+        return createFurnaceTicker(level, entity, ModBlockEntities.GAIA_STONE_FURNACE.get());
     }
 
     @Override
