50b900a24d4df75eb0910dc0643099ae478cc944
Andromander <androsa@protonmail.com>
2023-05-04 10:00:04 +1000

    Port menu registration

--- src/main/java/androsa/gaiadimension/registry/ModMenus.java
+++ src/main/java/androsa/gaiadimension/registry/ModMenus.java
@@ -6,6 +6,7 @@ import androsa.gaiadimension.block.screen.*;
 import androsa.gaiadimension.item.inventory.GemPouchContainer;
 import androsa.gaiadimension.item.inventory.GemPouchScreen;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -15,22 +16,22 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModMenus {
 
-    public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, GaiaDimensionMod.MODID);
+    public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.MENU_TYPES, GaiaDimensionMod.MODID);
 
     public static final RegistryObject<MenuType<AgateCraftingTableMenu>> AGATE_CRAFTING_TABLE = CONTAINERS.register(
-            "agate_crafting_table", () -> new MenuType<>(AgateCraftingTableMenu::new));
+            "agate_crafting_table", () -> new MenuType<>(AgateCraftingTableMenu::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<GaiaStoneFurnaceMenu>> GAIA_STONE_FURNACE = CONTAINERS.register(
-            "gaia_stone_furnace", () -> new MenuType<>(GaiaStoneFurnaceMenu::new));
+            "gaia_stone_furnace", () -> new MenuType<>(GaiaStoneFurnaceMenu::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<GemPouchContainer>> GEMSTONE_POUCH = CONTAINERS.register(
-            "gemstone_pouch", () -> new MenuType<>(GemPouchContainer::new));
+            "gemstone_pouch", () -> new MenuType<>(GemPouchContainer::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<SmallCrateContainer>> SMALL_CRATE = CONTAINERS.register(
-            "small_crate", () -> new MenuType<>(SmallCrateContainer::new));
+            "small_crate", () -> new MenuType<>(SmallCrateContainer::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<LargeCrateMenu>> LARGE_CRATE = CONTAINERS.register(
-            "large_crate", () -> new MenuType<>(LargeCrateMenu::new));
+            "large_crate", () -> new MenuType<>(LargeCrateMenu::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<RestructurerMenu>> RESTRUCTURER = CONTAINERS.register(
-            "restructurer", () -> new MenuType<>(RestructurerMenu::new));
+            "restructurer", () -> new MenuType<>(RestructurerMenu::new, FeatureFlags.REGISTRY.allFlags()));
     public static final RegistryObject<MenuType<PurifierMenu>> PURIFIER = CONTAINERS.register(
-            "purifier", () -> new MenuType<>(PurifierMenu::new));
+            "purifier", () -> new MenuType<>(PurifierMenu::new, FeatureFlags.REGISTRY.allFlags()));
 
     @OnlyIn(Dist.CLIENT)
     public static void registerScreens() {
