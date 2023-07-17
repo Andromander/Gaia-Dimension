526a3ee3c33f283bcde16937e31739bddef669fd
Andromander <androsa@protonmail.com>
2023-07-18 09:43:36 +1000

    Fully implement new features
    The most forgettable part of making new stuff: tabs, and lang entries

--- src/main/java/androsa/gaiadimension/registry/registration/ModTabs.java
+++ src/main/java/androsa/gaiadimension/registry/registration/ModTabs.java
@@ -41,7 +41,7 @@ public class ModTabs {
                 add(output, ModBlocks.scarlet_mookaite, ModBlocks.auburn_mookaite, ModBlocks.gold_mookaite, ModBlocks.mauve_mookaite, ModBlocks.beige_mookaite, ModBlocks.ivory_mookaite);
                 add(output, ModBlocks.golden_sand, ModBlocks.aurum_mud, ModBlocks.golden_stone, ModBlocks.tough_golden_stone, ModBlocks.brilliant_stone, ModBlocks.gilded_brilliant_stone);
                 add(output, ModBlocks.pebbles, ModBlocks.gaia_stone, ModBlocks.gaia_cobblestone, ModBlocks.primal_mass, ModBlocks.nexustone);
-                add(output, ModBlocks.speckled_rock, ModBlocks.coarse_rock, ModBlocks.precious_rock, ModBlocks.pyrite_ore, ModBlocks.sugilite_ore, ModBlocks.hematite_ore, ModBlocks.cinnabar_ore, ModBlocks.labradorite_ore, ModBlocks.moonstone_ore, ModBlocks.red_opal_ore, ModBlocks.blue_opal_ore, ModBlocks.green_opal_ore, ModBlocks.white_opal_ore);
+                add(output, ModBlocks.speckled_rock, ModBlocks.coarse_rock, ModBlocks.precious_rock, ModBlocks.scarlet_opalite_ore, ModBlocks.auburn_opalite_ore, ModBlocks.gold_opalite_ore, ModBlocks.mauve_opalite_ore, ModBlocks.beige_opalite_ore, ModBlocks.ivory_opalite_ore, ModBlocks.pyrite_ore, ModBlocks.sugilite_ore, ModBlocks.hematite_ore, ModBlocks.cinnabar_ore, ModBlocks.labradorite_ore, ModBlocks.moonstone_ore, ModBlocks.red_opal_ore, ModBlocks.blue_opal_ore, ModBlocks.green_opal_ore, ModBlocks.white_opal_ore);
                 add(output, ModBlocks.pink_agate_tiles, ModBlocks.pink_agate_tile_stairs, ModBlocks.pink_agate_tile_slab);
                 add(output, ModBlocks.blue_agate_tiles, ModBlocks.blue_agate_tile_stairs, ModBlocks.blue_agate_tile_slab);
                 add(output, ModBlocks.green_agate_tiles, ModBlocks.green_agate_tile_stairs, ModBlocks.green_agate_tile_slab);
@@ -79,7 +79,7 @@ public class ModTabs {
                 add(output, ModItems.crystal_shard, ModItems.agate_stick, ModItems.agate_fabric, ModItems.sugar_crystals, ModItems.sugar_cluster, ModItems.sturdy_pebble, ModItems.shiny_bone, ModItems.fine_thread, ModItems.sweet_muckball, ModItems.cloudy_shard);
                 add(output, ModItems.agate_cup, ModItems.twined_thread, ModItems.pink_essence, ModItems.pink_goo, ModItems.scaynyx_ingot);
                 add(output, ModItems.goldstone_residue, ModItems.goldstone, ModItems.bismuth_residue, ModItems.bismuth_crystal, ModItems.aura_residue, ModItems.aura_cluster, ModItems.black_residue, ModItems.tektite);
-                add(output, ModItems.pyrite, ModItems.sugilite, ModItems.hematite, ModItems.cinnabar, ModItems.labradorite, ModItems.moonstone, ModItems.red_opal, ModItems.blue_opal, ModItems.green_opal, ModItems.white_opal);
+                add(output, ModItems.opalite, ModItems.pyrite, ModItems.sugilite, ModItems.hematite, ModItems.cinnabar, ModItems.labradorite, ModItems.moonstone, ModItems.red_opal, ModItems.blue_opal, ModItems.green_opal, ModItems.white_opal);
                 add(output, ModItems.ixiolite, ModItems.proustite, ModItems.euclase, ModItems.leucite, ModItems.carnelian, ModItems.benitoite, ModItems.diopside, ModItems.chalcedony);
                 add(output, ModItems.pyrite_powder, ModItems.hematite_powder, ModItems.cinnabar_powder, ModItems.labradorite_powder, ModItems.moonstone_powder, ModItems.red_opal_powder, ModItems.blue_opal_powder, ModItems.green_opal_powder, ModItems.white_opal_grit);
                 add(output, ModItems.tilipi, ModItems.tilibl, ModItems.tiligr, ModItems.tilipu, ModItems.tiliol, ModItems.tilimy, ModItems.plagued_tiliey, ModItems.tiliou);
