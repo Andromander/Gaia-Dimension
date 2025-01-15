04cf0317d9b184066688b3dc46ca88dd55f57f9e
Andromander <androsa@protonmail.com>
2025-01-15 20:51:08 +1100

    Fix missing registry

--- src/main/java/androsa/gaiadimension/GaiaDimensionMod.java
+++ src/main/java/androsa/gaiadimension/GaiaDimensionMod.java
@@ -64,6 +64,7 @@ public class GaiaDimensionMod {
         ModItems.ITEMS.register(bus);
         ModParticles.PARTICLE_TYPES.register(bus);
         ModPredicates.ENTITY_SUB_PREDICATES.register(bus);
+        ModRecipes.RECIPE_BOOK_CATEGORIES.register(bus);
         ModRecipes.RECIPE_TYPES.register(bus);
         ModRecipes.RECIPE_SERIALIZERS.register(bus);
         ModBlockEntities.TILE_ENTITIES.register(bus);
