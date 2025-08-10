3d7dc8481a3471c720af9e600a5c3cdf983ffacb
Andromander <androsa@protonmail.com>
2025-08-11 08:36:10 +1000

    Fix #145

--- src/main/java/androsa/gaiadimension/data/GaiaRecipes.java
+++ src/main/java/androsa/gaiadimension/data/GaiaRecipes.java
@@ -281,8 +281,10 @@ public class GaiaRecipes extends GaiaRecipeProvider {
                 .define('#', GaiaTags.Items.TILES)
                 .unlockedBy("has_tiles", has(GaiaTags.Items.TILES))
                 .save(consumer, loc("agate_cup"));
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.agate_stick.get(), 4)
-                .requires(GaiaTags.Items.TILES)
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.agate_stick.get(), 4)
+                .pattern("#")
+                .pattern("#")
+                .define('#', GaiaTags.Items.TILES)
                 .unlockedBy("has_tiles", has(GaiaTags.Items.TILES))
                 .save(consumer, loc("agate_stick"));
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.aura_rod.get(), 4)
