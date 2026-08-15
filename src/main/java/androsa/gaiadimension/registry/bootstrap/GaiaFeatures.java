dc0f413bd281872d8ea91691f2de06e7d8461012
Andromander <androsa@protonmail.com>
2026-08-15 16:16:50 +1000

    Bump NF

--- src/main/java/androsa/gaiadimension/registry/bootstrap/GaiaFeatures.java
+++ src/main/java/androsa/gaiadimension/registry/bootstrap/GaiaFeatures.java
@@ -30,7 +30,6 @@ import net.minecraft.util.valueproviders.ClampedInt;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.util.valueproviders.UniformInt;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.grower.TreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
